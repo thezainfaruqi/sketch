e37e8292c9467c2b227ce34e52d655f58f0dd780
Zain <zainfaruqi9@gmail.com>
2025-02-22 02:03:32 -0500

    added a change grid size button

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 const container = document.getElementById("grid");
-for (let i = 0; i < 16 * 16; i++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    container.appendChild(square);
+
+function createGrid(size) {
+
+    container.innerHTML = ""; 
+
+    for (let i = 0; i < size * size; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        container.appendChild(square);
+    }
+
 }
+
+const button = document.getElementById("button")
+button.addEventListener("click", function() {
+    let gridSize = parseInt(prompt("Please enter grid size (e.g., 2 for 2x2):"), 10);
+
+    if (gridSize < 0 | gridSize > 100) {
+        alert("Please enter a valid number between 1 and 100.")
+        return
+    }
+
+    createGrid(gridSize)
+})
+
+createGrid(16) // The normal
